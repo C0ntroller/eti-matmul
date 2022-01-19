a088a4eb99f0fc13e8df87eaca3e9c7615e1082a
Daniel <43993811+C0ntroller@users.noreply.github.com>
2022-01-19 16:42:53 +0100

    Add blocking

--- uebung_2/matmul.c
+++ uebung_2/matmul.c
@@ -19,18 +19,12 @@ void matmul_swapped_loops(double* A, double* B, double* C, int n){
                 C[i*n + j] += A[i*n + k] * B[k*n + j];
 }
 
-void matmul_constants(double* A, double* B, double* C, int n){
-    for (int i = 0; i < n; i++)
-        for (int k = 0; k < n; k++)
-            double temp_a = A[i*n + k]
-            for (int j = 0; j < n; j++)
-                C[i*n + j] += temp_a * B[k*n + j];
-}
 
 void matmul_unrolled_loops(double* A, double* B, double* C, int n){
+    int unrollsize = 2; //2,4,8,16,32,64
     for (int i = 0; i < n; i++) {
         for (int k = 0; k < n; k++) {
-            for (int j = 0; j < n; j += 2) { //2,4,8,16,32,64
+            for (int j = 0; j < n; j += unrollsize) { 
                 C[i * n + j] += A[i * n + k] * B[k * n + j];
                 C[i * n + j + 1] += A[i * n + k] * B[k * n + j + 1];
             }
@@ -38,12 +32,64 @@ void matmul_unrolled_loops(double* A, double* B, double* C, int n){
     }
 }
 
+void matmul_blocking(double* A, double* B, double* C, int n){
+    int tilesize = 2; //2,4,8,16,32,64
+    int unrollsize = 2; //2,4,8,16,32,64
+    for (int i1 = 0; i1 < n; i1 += tilesize) {
+        for (int k1 = 0; k1 < n; k1 += tilesize) {
+            for (int i2 = 0; i2 < tilesize; i++) {
+               for (int k2 = 0; k2 < tilesize; k++) { 
+                   for (int j = 0; j < n; j += unrollsize) { 
+                    C[(i1 + i2) * n + j] += A[(i1 + i2) * n + (k1 + k2)] * B[(k1 + k2) * n + j];
+                    C[(i1 + i2) * n + j + 1] += A[(i1 + i2) * n + (k1 + k2)] * B[(k1 + k2) * n + j + 1];
+                }
+            }
+        }
+    }
+}
+
 void matmul_pointer(double* A, double* B, double* C, int n){
-    for (int i = 0; i < n; i++) {
+    /*for (int i = 0; i < n; i++) {
         for (int k = 0; k < n; k++) {
             for (int j = 0; j < n; j += 2) { //2,4,8,16,32,64
-                C[i * n + j] += A[i * n + k] * B[k * n + j];
+                *(C + i * n + j) += *(A + i * n + k) * *(B + k * n + j);
                 *(C + i * n + j + 1) += *(A + i * n + k) * *(B + k * n + j + 1);
+            }
+        }
+    }*/
+    int tilesize = 2; //2,4,8,16,32,64
+    int unrollsize = 2; //2,4,8,16,32,64
+    for (int i1 = 0; i1 < n; i1 += tilesize) {
+        for (int k1 = 0; k1 < n; k1 += tilesize) {
+            for (int i2 = 0; i2 < tilesize; i++) {
+               for (int k2 = 0; k2 < tilesize; k++) { 
+                   for (int j = 0; j < n; j += unrollsize) { 
+                    *(C + (i1 + i2) * n + j) += *(A + (i1 + i2) * n + (k1 + k2)) * *(B + (k1 + k2) * n + j);
+                    *(C + (i1 + i2) * n + j + 1) += *(A + (i1 + i2) * n + (k1 + k2)) * *(B + (k1 + k2) * n + j + 1);
+                }
+            }
+        }
+    }
+}
+
+void matmul_constants(double* A, double* B, double* C, int n){
+    /*for (int i = 0; i < n; i++)
+        for (int k = 0; k < n; k++) {
+            double temp_a = *(A + i * n + k)
+            for (int j = 0; j < n; j++)
+                *(C + i * n + j + 1) += temp_a * *(B + k * n + j + 1);
+        }*/
+    int tilesize = 2; //2,4,8,16,32,64
+    int unrollsize = 2; //2,4,8,16,32,64
+    for (int i1 = 0; i1 < n; i1 += tilesize) {
+        for (int k1 = 0; k1 < n; k1 += tilesize) {
+            for (int i2 = 0; i2 < tilesize; i++) {
+                for (int k2 = 0; k2 < tilesize; k++) { 
+                    double temp_a = *(A + (i1 + i2) * n + (k1 + k2));
+                    for (int j = 0; j < n; j += unrollsize) { 
+                        *(C + (i1 + i2) * n + j) += temp_a * *(B + (k1 + k2) * n + j);
+                        *(C + (i1 + i2) * n + j + 1) += temp_a * *(B + (k1 + k2) * n + j + 1);
+                }
             }
         }
     }
