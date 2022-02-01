65f1e0f8b56f18400e326a673625a2e76750df95
Leah <leah.sieder@gmx.de>
2022-02-01 20:18:03 +0100

    Revised mpi version and added measured runtimes.

--- uebung_3/matmul.c
+++ uebung_3/matmul.c
@@ -34,7 +34,7 @@ int results_correct(double* A, double* B, int n){
 
 int main(int args, char *argsv[]) {
     int rank, size;
-    MPI_Init(NULL, NULL);
+    MPI_Init(&args, &argsv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
@@ -47,27 +47,15 @@ int main(int args, char *argsv[]) {
     sscanf(argsv[1], "%d", &n);
     srand(time(NULL));
 
-    int rows = (int) n / size;
-    int bfrsize = n * rows;
-
-    int* slices = (int *) malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++) {
-        slices[i] = bfrsize;
-    } 
-
-    int* offsets = (int *) malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++) {
-        offsets[i] = bfrsize * i;
-    }
-
-    double *ptr_A, *ptr_B, *ptr_C, *ptr_A_tile, *ptr_C_result, *ptr_C_check;
+    // Allocate matrices
+    double *ptr_A, *ptr_B, *ptr_C, *ptr_A_local, *ptr_C_local, *ptr_C_check;
+    ptr_A = (double *) malloc(n * n * sizeof(double));
     ptr_B = (double *) malloc(n * n * sizeof(double));
+    ptr_C = (double *) malloc(n * n * sizeof(double));
+    ptr_C_check = (double *) malloc(n * n * sizeof(double));
 
+    // Master process
     if (rank == 0) {
-        ptr_A = (double *) malloc(n * n * sizeof(double));
-        ptr_C = (double *) malloc(n * n * sizeof(double));
-        ptr_C_check = (double *) malloc(n * n * sizeof(double));
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 ptr_A[i * n + j] = (double) ((rand() % 2000) + 9000) / 10000;
@@ -77,68 +65,82 @@ int main(int args, char *argsv[]) {
             }
         }
     }
-    
-    ptr_A_tile = (double *) malloc(bfrsize * sizeof(double));
-    //ptr_B_tile = (double *) malloc(bfrsize * sizeof(double));
-    ptr_C_result = (double *) malloc(bfrsize * sizeof(double));
+
+    // Row distribution
+    int* slices = (int *) malloc(size * sizeof(int));
+    int* offsets = (int *) malloc(size * sizeof(int));
+
+    int offset = 0;
+    for (int i = 0; i < size; i++){
+        offsets[i] = offset;
+        slices[i] = (int) (n/size);
+        if(i < n%size) slices[i]++;
+        offset += slices[i];
+
+        slices[i] = slices[i] * n;
+        offsets[i] = offsets[i] * n;
+    }
+
+    // Allocate local matrices
+    ptr_A_local = (double *) malloc(slices[rank] * sizeof(double));
+    ptr_C_local = (double *) malloc(slices[rank] * sizeof(double));
+    for(int i = 0; i < slices[rank]; i++){
+        ptr_C_local[i] = 0.0;
+    }
 
     double runtime;
     struct timeval tv1, tv2;
 
-    if(rank == 0) matmul_swapped_loops(ptr_A, ptr_B, ptr_C_check, n);
+    if(rank == 0)
+        matmul_swapped_loops(ptr_A, ptr_B, ptr_C_check, n);
 
     for (int i = 0; i < 10; i++) {
         MPI_Barrier(MPI_COMM_WORLD);
-        gettimeofday(&tv1, NULL);
-        MPI_Scatterv(ptr_A, slices, offsets, MPI_DOUBLE, ptr_A_tile, bfrsize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        if(rank == 0) gettimeofday(&tv1, NULL);
+
+        MPI_Scatterv(ptr_A, slices, offsets, MPI_DOUBLE, ptr_A_local, slices[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
         MPI_Bcast(ptr_B, n*n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-        matmul_mpi(ptr_A_tile, ptr_B, ptr_C_result, rows, n);
-        MPI_Gatherv(ptr_C_result, bfrsize, MPI_DOUBLE, ptr_C, slices, offsets, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-        if(rank == 0 && n - (rows * size) != 0) {
-            matmul_mpi(ptr_A + (n * rows * size), ptr_B, ptr_C + (n * rows * size), n % size, n);
-        }
-        gettimeofday(&tv2, NULL);
 
-        if (rank == 0 && !results_correct(ptr_C, ptr_C_check, n)) {
-            printf("Incorrect\n");
-            /*for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    printf("%f\t", ptr_C[n*i + j]);
-                }
-                printf("\n");
+        matmul_mpi(ptr_A_local, ptr_B, ptr_C_local, (slices[rank]/n), n);
+
+        MPI_Gatherv(ptr_C_local, slices[rank], MPI_DOUBLE, ptr_C, slices, offsets, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        if(rank == 0)
+            gettimeofday(&tv2, NULL);
+
+        if (rank == 0){
+            if (!results_correct(ptr_C, ptr_C_check, n)){
+                printf("Incorrect\n");
+                return 1;
+            } else{
+                printf("correct\n");
             }
-
-            printf("\n");
-            printf("\n");
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    printf("%f\t", ptr_C_check[n*i + j]);
-                }
-                printf("\n");
-            }*/
-
-            return 1;
         }
 
-        runtime = (double) (tv2.tv_usec - tv1.tv_usec) / 1000 + 1000 * (double) (tv2.tv_sec - tv1.tv_sec);
-        if (rank == 0) printf("%f,", runtime);
 
-        memset(ptr_C_result, 0, bfrsize * sizeof(double));
+        if (rank == 0){
+            runtime = (double) (tv2.tv_usec - tv1.tv_usec) / 1000 + 1000 * (double) (tv2.tv_sec - tv1.tv_sec);
+            printf("%f,", runtime);
+        }
+
+        memset(ptr_C_local, 0, slices[rank] * sizeof(double));
         if (rank == 0) memset(ptr_C, 0, n*n * sizeof(double));
     }
 
-    if (rank == 0) {
-        free(ptr_A);
-        //free(ptr_B);
-        free(ptr_C);
-        free(ptr_C_check);
-    }
-    free(ptr_A_tile);
+    // Free allocated memory
+    free(ptr_A);
     free(ptr_B);
-    free(ptr_C_result);
+    free(ptr_C);
+    free(ptr_C_check);
+    free(ptr_A_local);
+    free(ptr_C_local);
+    free(slices);
+    free(offsets);
 
     MPI_Finalize();
+
     if(rank==0) printf("\n");
+
     return 0;
 }
